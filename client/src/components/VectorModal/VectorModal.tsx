0435271e189df40bb0ff1ed03b3b41288c0bce81
Jesus Mejias Garcia <jmejiasgarcia@deloitte.com>
2024-08-08 09:33:03 -0400

    Fixing console errors and modal not showing the correct screen

--- client/src/components/VectorModal/VectorModal.tsx
+++ client/src/components/VectorModal/VectorModal.tsx
@@ -246,7 +246,7 @@ export const VectorModal = ({
     const firstStep = () => {
         return (
             <>
-                {selectedVectorDB &&
+                {selectedVectorDB || selectedVectorDB === undefined &&
                     <>
                         <StyledButtonGroup>
                             <IconButton onClick={() => setOpen(false)}>
@@ -293,7 +293,7 @@ export const VectorModal = ({
                         </SansTypography>
                     </>
                 }
-                {!selectedVectorDB && <StyledTitle variant="h6">
+                {selectedVectorDB !== undefined && <StyledTitle variant="h6">
                     Upload Files
                 </StyledTitle>}
                 <Dropzone
